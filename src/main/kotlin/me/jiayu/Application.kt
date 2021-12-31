768cd28177f43af665a258c8e1323306148549cf
Jiayu Liu <jiayu@hey.com>
2021-12-31 13:51:55 +0800

    serial

--- src/main/kotlin/me/jiayu/Application.kt
+++ src/main/kotlin/me/jiayu/Application.kt
@@ -1,15 +1,36 @@
 package me.jiayu
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.routing.*
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.http.toHttpDateString
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.serialization.json
+import io.ktor.server.cio.EngineMain
+import kotlinx.serialization.Serializable
+import java.time.LocalDateTime
+import java.time.ZonedDateTime
+import java.util.concurrent.ThreadLocalRandom
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>): Unit = EngineMain.main(args)
+
+@Serializable
+data class TimeInfo(val time: String, val rand: Int)
 
 fun Application.module(testing: Boolean = false) {
     routing {
+        install(ContentNegotiation) {
+            json()
+        }
         get("/") {
             call.respondText("Hello, world!")
+        }
+        get("/time") {
+            call.respond(TimeInfo(ZonedDateTime.now().toHttpDateString(), rand = ThreadLocalRandom.current().nextInt()))
         }
     }
 }
